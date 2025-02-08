ad783f6ec62c178635f3732c27067030c8c58d23
Tanner Foster <tannerfoster@ferristexas.gov>
2025-02-07 22:52:26 -0600

    Flatburger Scope Lab Submission

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+const burgers = ['Hamburger', 'Cheeseburger'];
+const fearturedDrink = 'Strawberry Milkshake';
+
+function addBurger() {
+    const newBurger = 'Flatburger';
+    burgers.push(newBurger);
+
+    if(newBurger == 'Flatburger') {
+        const anothernewBurger = 'Maple Bacon Burger';
+        burgers.push(anothernewBurger);
+    }
+    
+}
+
+function changeFeaturedDrink() {
+    fearturedDrink = 'The JavaShake';
+}
+
